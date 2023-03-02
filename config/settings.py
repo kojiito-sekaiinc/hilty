8277a05d09440c98928f30d73417c8818d15bf9d
kojiito-sekaiinc <koji.ito@sekai-inc.jp>
2023-03-02 16:38:47 +0900

    local_settings.py add

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qx2$_tx*qy7jfl&617!!^4*%w1dq5^y^-70_2nluu-0=(c6d0k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
