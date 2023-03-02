03e1fbb6ec6a865d92748156a58b8e4b6a07c0d2
kojiito-sekaiinc <koji.ito@sekai-inc.jp>
2023-03-02 16:54:16 +0900

    settings.py update

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -133,3 +133,6 @@ try:
 except ImportError:
     pass
 
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
